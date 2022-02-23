26592f67494e94a7fbe952b0bf1a53d4ea4664a5
Teopka <99753679+Teopka@users.noreply.github.com>
2022-02-23 20:15:10 +0200

    Add files via upload

---
+++ Work 2.js
@@ -0,0 +1,8 @@
+'use strict';
+let myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array": [7, 9, { purpose: "путаница", number: 123 }, 3.3], "random animal": "Банановая акула"
+  };
+  
+  console.log(myCrazyObject["some array"][2].number);
+  
